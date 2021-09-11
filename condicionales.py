55294a2bae4abe9ea106625eab9e41362379bf8c
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-09-10 22:43:20 -0500

    ultimo commit - 3 numeros y uno mayor

--- condicionales.py
+++ condicionales.py
@@ -193,3 +193,18 @@ else:
     suma = num1 + num2
     print(f'se sumaron y dio {suma}')
     
+"""
+Leer tres números diferentes e imprimir el número mayor de los
+tres.
+"""
+
+num1 = float(input('ingrese el primer numero: '))
+num2 = float(input('ingrese el segundo numero: '))
+num3 = float(input('ingrese el tercero numero: '))
+
+if num1 > num2 > num3:
+    print(f'{num1}')
+if num2 > num1 > num3:
+    print(f'{num2}')
+if num3 > num2 > num1:
+    print(f'{num3}')

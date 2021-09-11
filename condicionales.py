355af3c1c22384fce8fdfbfaefd6568842582684
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-09-10 21:00:44 -0500

    ejercicio 1 - camisas y descuentos

--- condicionales.py
+++ condicionales.py
@@ -5,3 +5,14 @@ Created on Fri Sep 10 20:48:14 2021
 @author: jeha2
 """
 
+"""
+Hacer un algoritmo que calcule el total a pagar por la compra de
+camisas. Si se compran tres camisas o mas se aplica un descuento
+del 30% sobre el total de la compra y si son menos de tres camisas
+un descuento del 10%.
+"""
+compra = int(input('¿Cuantas camisas desea comprar? : '))
+if compra >= 3:
+    print('se aplicara un descuento del 30%')
+else:
+    print('se aplicara un descuento del 10%')
